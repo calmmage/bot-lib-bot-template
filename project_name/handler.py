980879345caa946fc1491ddd4d606403cd61800e
Petr Lavrov <petrlavrov@calmmage.com>
2024-07-14 18:06:35 +0300

    update send_safe, reply_safe, answer_safe argument order to be consistent with aiogram

--- project_name/handler.py
+++ project_name/handler.py
@@ -15,8 +15,8 @@ class MyHandler(Handler):
     async def chat_handler(self, message, app: MyApp):
         input_str = await self.get_message_text(message)
         output_str = app.invoke(input_str)
-        await self.reply_safe(output_str, message)
+        await self.reply_safe(message, output_str)
 
     async def dummy_command_handler(self, message, app: MyApp):
         output_str = app.dummy_command()
-        await self.reply_safe(output_str, message)
+        await self.reply_safe(message, output_str)
