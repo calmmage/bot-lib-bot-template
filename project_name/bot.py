801d9686e97803c67af4d0ad6e6fc9df8ff9bd22
Reliable Magician <petrlavrov@calmmage.com>
2024-09-14 01:59:37 +0300

    reorder imports and env

--- project_name/bot.py
+++ project_name/bot.py
@@ -1,10 +1,12 @@
 from aiogram import Dispatcher
-from dotenv import load_dotenv
-
 from bot_lib import BotManager
 from bot_lib.utils import create_bot
+from dotenv import load_dotenv
+
 from project_name.app import MyApp
 from project_name.handler import MyHandler
+
+load_dotenv()
 
 app = MyApp()
 bot_manager = BotManager(app=app)
@@ -15,7 +17,6 @@ my_handler = MyHandler()
 handlers = [my_handler]
 bot_manager.setup_dispatcher(dp, extra_handlers=handlers)
 
-load_dotenv()
 bot = create_bot()
 
 if __name__ == "__main__":
